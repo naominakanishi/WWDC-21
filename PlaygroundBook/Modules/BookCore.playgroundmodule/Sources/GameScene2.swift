b340d188b249e54cfea0da306e2eabd082b98975
Naomi Nakanishi <naomi.nknsh@gmail.com>
2021-04-13 18:03:38 -0300

    added top shopping scene

--- PlaygroundBook/Modules/BookCore.playgroundmodule/Sources/GameScene2.swift
+++ PlaygroundBook/Modules/BookCore.playgroundmodule/Sources/GameScene2.swift
@@ -2,12 +2,17 @@ import SpriteKit
 
 public class GameScene2: SKScene {
 
-    lazy var happyEarth = childNode(withName: "happyEarth") as! SKSpriteNode
-    var dialogs = ["Buying new clothes can be fun, right?", "Have you ever considered the impact your clothes' production has on the environment, though?"]
+    var selectedTop: SKSpriteNode?
+    lazy var capSleeveShirt = childNode(withName: "capSleeveShirt") as! SKSpriteNode
+    lazy var cottonTshirt = childNode(withName: "cottonTshirt") as! SKSpriteNode
+    lazy var knitPullover = childNode(withName:"knitPullover") as! SKSpriteNode
+    lazy var nextButton = childNode(withName:"nextButton") as! SKSpriteNode
     
     public override func didMove(to view: SKView){
-        let fadeInAction = SKAction.fadeIn(withDuration: 1)
-        happyEarth.run(fadeInAction)
+        let fadeInAction = SKAction.fadeIn(withDuration: 0.5)
+        capSleeveShirt.run(fadeInAction)
+        cottonTshirt.run(fadeInAction)
+        knitPullover.run(fadeInAction)
     }
 
     //text.text = "frase aqui"
@@ -21,7 +26,22 @@ public class GameScene2: SKScene {
 
     // Shows first balloon with message "Hi" (default)
     func touchDown(atPoint pos : CGPoint) {
-       
+        if capSleeveShirt.contains(pos) && capSleeveShirt.alpha == 1{
+            selectedTop = capSleeveShirt
+        } else if cottonTshirt.contains(pos) && capSleeveShirt.alpha == 1{
+            selectedTop = cottonTshirt
+        } else if knitPullover.contains(pos) && knitPullover.alpha == 1{
+            selectedTop = knitPullover
+        }
+        if selectedTop != nil{
+            nextButton.alpha = 1
+        }
+        if nextButton.alpha == 1{
+            if let nextGameScene = GameScene3(fileNamed: "GameScene3") {
+              nextGameScene.scaleMode = .aspectFit
+              self.scene?.view?.presentScene(nextGameScene, transition: SKTransition.fade(withDuration: TimeInterval(1)))
+            }
+        }
     }
     
     func touchMoved(toPoint pos : CGPoint) {
@@ -52,4 +72,3 @@ public class GameScene2: SKScene {
         // Called before each frame is rendered
     }
 }
-
